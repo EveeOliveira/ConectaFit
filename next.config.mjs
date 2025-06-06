104ec6333783eb90595d6a622cfbf16d29f8ba81
Evelen <evelenpinheiro62@gmail.com>
2025-06-05 22:39:50 -0300

    build

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,11 @@ const nextConfig = {
     unoptimized: true,
   },
   distDir: 'dist',
+  // Configuração do servidor
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
 }
 
 export default nextConfig
